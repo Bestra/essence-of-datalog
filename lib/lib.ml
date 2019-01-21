c2fc378ca43bbeeb75b4caa1b2082ccb113b7037
Chris Westra <cwestra@gmail.com>
2019-01-21 17:32:13 -0500

    unification

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,36 +1,36 @@
 open Base
+
 (* data Rule = Rule { _head :: Atom, _body :: [ Atom ] }
-data Atom = Atom { _predSym :: String, _terms :: [ Term ] } deriving Eq
-data Term = Var String | Sym String deriving Eq *)
+   data Atom = Atom { _predSym :: String, _terms :: [ Term ] } deriving Eq
+   data Term = Var String | Sym String deriving Eq *)
 
 type term = Var of string | Sym of string
-type atom = {
-  predSym: string; terms: term list
-}
+
+type atom = {predSym: string; terms: term list}
 
 type rule = {head: atom; body: atom}
 
 type program = rule list
 
 type knowledge_base = atom list
+
 type subsitution = (term * term) list
 
 let empty_substitution = []
 
 (* substitute :: Atom -> Substitution -> Atom
-substitute atom substitution = atom { _terms = map go (_terms atom) }
-  where
-  go sym@Sym{} = sym
-  go var@Var{} = fromMaybe var (var `lookup` substitution) *)
+   substitute atom substitution = atom { _terms = map go (_terms atom) }
+   where
+   go sym@Sym{} = sym
+   go var@Var{} = fromMaybe var (var `lookup` substitution) *)
 let substitute atom substitution =
   let go = function
-  | Sym _ as s -> s
-  | Var _ as v -> 
-    let sub = List.Assoc.find substitution v ~equal:(phys_equal) in
-    Option.value sub ~default:v
+    | Sym _ as s -> s
+    | Var _ as v ->
+        let sub = List.Assoc.find substitution v ~equal:phys_equal in
+        Option.value sub ~default:v
   in
-
-  {atom with terms=(List.map ~f:go atom.terms)}
+  {atom with terms= List.map ~f:go atom.terms}
 
 (* 
 unify :: Atom -> Atom -> Maybe Substitution
@@ -48,4 +48,17 @@ unify (Atom predSym ts) (Atom predSym' ts')
       _                   -> return $ (v,s) : incompleteSubstitution
   go ((_, Var{}) : _) = error "The second atom is assumed to be ground." *)
 
-let unify a1 a2 =
+let unify {predSym; terms} {predSym= predSym'; terms= terms'} =
+  let open Option.Let_syntax in
+  let rec go = function
+    | [] -> Some empty_substitution
+    | ((Sym _ as s), (Sym _ as s')) :: rest ->
+        if phys_equal s s' then go rest else None
+    | ((Var _ as v), (Sym _ as s)) :: rest -> (
+        let%bind incomplete_substitution = go rest in
+        match List.Assoc.find incomplete_substitution v ~equal:phys_equal with
+        | Some s' when not (phys_equal s s') -> None
+        | _ -> Some ((v, s) :: incomplete_substitution) )
+    | (_, Var _) :: _ -> failwith "Foo"
+  in
+  if phys_equal predSym predSym' then List.zip_exn terms terms' |> go else None
